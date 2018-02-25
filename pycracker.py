fcc1a70ce07dde0da35017e5016e3d0f9d0cacda
Pedro Pereira <pedro000pereira@gmail.com>
2018-02-25 21:22:13 +0000

    Update pycracker.py

--- pycracker.py
+++ pycracker.py
@@ -4,12 +4,12 @@ import pycracker_def as func_lib
 
 def main():
 	func_lib.arg_Parser()
-	var = func_lib.arg_Parser.options
-	var1 = func_lib.arg_Parser.usg
-	if var.user == None:
-		func_lib.no_User(var, var1 )
+	parser = func_lib.arg_Parser.options
+	usage = func_lib.arg_Parser.usg
+	if parser.user == None:
+		func_lib.no_User(parser, usage )
 	else:
-		func_lib.w_User(var, var1 )
+		func_lib.w_User(parser, usage )
 
 
 if __name__ == "__main__":
